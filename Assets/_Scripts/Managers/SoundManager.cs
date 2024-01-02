33e8a5f51d4980e74f6c36fd6a14520d47e2b53c
ZANE <130233619+chochozane@users.noreply.github.com>
2024-01-02 12:59:11 +0900

    [EDIT] Script - SoundManager
    싱글톤화 작업 및 초기 볼륨값 세팅

--- Assets/_Scripts/Managers/SoundManager.cs
+++ Assets/_Scripts/Managers/SoundManager.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    [SerializeField] private AudioSource audioSource; // 음악 플레이할 친구
+    public static SoundManager instance;
+
+    private AudioSource audioSource; // 음악 플레이할 친구
     [SerializeField] private AudioClip bgm; // 음악 파일 그 자체
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            audioSource = GetComponent<AudioSource>();
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            if (instance != this)
+            {
+                Destroy(this);
+            }
+        }
+    }
 
     // Start is called before the first frame update
     private void Start()
     {
         audioSource.clip = bgm;
+        audioSource.volume = 0.5f;
         audioSource.Play();
     }
 
