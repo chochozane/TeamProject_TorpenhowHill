6b0baff7967cbdd89cbb1d08fc6c9449097e7405
조성우 <chomuscleguy@gmail.com>
2024-01-05 21:31:34 +0900

    [FEAT] village frame

---
+++ Assets/_ScriptableObject/Scripts/QuestData.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ResourceType
+{
+    Stone,
+    Wood,
+    Grass,
+}
+
+[Serializable]
+public class RequiredResource
+{
+    public ResourceType resourceType;
+    public int requiredAmount;
+}
+
+[CreateAssetMenu(fileName = "NewQuest", menuName = "Quest System/Quest")]
+public class QuestData : ScriptableObject
+{
+    [Header("Info")]
+    public string questTitle;
+    public string questDescription;
+    public bool isCompleted;
+
+    public RequiredResource[] requiredResource;
+}
--- Assets/_Scripts/Resources/ResourceSpawner.cs
+++ Assets/_Scripts/Resources/ResourceSpawner.cs
@@ -1,14 +1,24 @@
+using System.Collections;
 using UnityEngine;
 
 public class ResourceSpawner : MonoBehaviour
 {
-    public ObjectPool objectPool; // 오브젝트 풀링을 위한 ObjectPool 스크립트 참조
-    public Transform spawnPoint; // 자원이 생성될 위치
-    public float spawnRadius = 3f; // 스폰 위치 주변 반경
+    public ObjectPool objectPool;
+    public Transform spawnPoint;
+    public float spawnRadius = 3f;
 
     void Start()
     {
-        SpawnResource();
+        StartCoroutine(SpawnCoroutine());
+    }
+
+    IEnumerator SpawnCoroutine()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(1f);
+            SpawnResource();
+        }
     }
 
     void SpawnResource()
@@ -17,17 +27,12 @@ public class ResourceSpawner : MonoBehaviour
 
         int randomPrefabIndex = Random.Range(0, objectPool.prefabs.Count);
 
-        // objectPool를 통해 오브젝트를 가져옴
         GameObject resource = objectPool.GetObjectFromPool(randomPrefabIndex);
 
         if (resource != null)
         {
             resource.transform.position = randomPosition;
             resource.SetActive(true);
-        }
-        else
-        {
-            Debug.LogWarning("Resource not assigned or available in the pool!");
         }
     }
 }
