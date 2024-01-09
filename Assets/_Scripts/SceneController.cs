d7975ac782d286866f8bd411379f8505f4323481
조성우 <chomuscleguy@gmail.com>
2024-01-09 11:10:40 +0900

    [ADD] ResetQuest

--- Assets/_Scripts/SceneController.cs
+++ Assets/_Scripts/SceneController.cs
@@ -5,9 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    public QuestData[] questDatas;
     public void StartToMainScene()
     {
         SoundManager.instance.uiSound.ClickAudio();
         SceneManager.LoadScene("MainScene");
+    }
+
+     private void ResetQuest()
+    {
+        for (int i = 0; i < questDatas.Length; i++)
+        {
+            questDatas[i].onGoing = false;
+            questDatas[i].isCompleted = false;
+        }
     }
 }
